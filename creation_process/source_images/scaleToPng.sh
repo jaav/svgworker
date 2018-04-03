e0785b41d50f1cf69ca38e7a9b4fa9d9953da59a
jaav <jef@virtualsushi.be>
2018-04-03 07:09:36 +0200

    Creating the 'scale to png' script

--- creation_process/source_images/scaleToPng.sh
+++ creation_process/source_images/scaleToPng.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 cd ../cleaned_source_images/man
-for file in ./*_cleaned_NS.svg; do
-	echo "$file"
-	#echo "${file/.svg/_changed.txt}" FOR RENAMING
-	svgexport "$file" "${file/_cleaned_NS.svg/_fortina.png}" png 80% 9922:13000 992:1300
-	convert "${file/_cleaned_NS.svg/_fortina.png}" -trim "${file/_cleaned_NS.svg/_cropped_fortina.png}"
-	convert "${file/_cleaned_NS.svg/_cropped_fortina.png}" -resize '256x256>' -gravity center -background transparent -extent 264x264 "${file/_cleaned_NS.svg/_cropped_resized_fortina.png}"
+for file in ./*_cleaned.svg; do
+	echo "exporting $file to man's folder"
+	svgexport "$file" ../../png_scaled_images/man/"${file/_cleaned.svg/_fortina.png}" png 80% 9922:13000 992:1300
 
 done
 cd ../woman
-for file in ./*_cleaned_NS.svg; do
-	echo "$file"
-	svgexport "$file" "${file/_cleaned_NS.svg/_fortina.png}" png 80% 9922:13000 992:1300
-	convert "${file/_cleaned_NS.svg/_fortina.png}" -trim "${file/_cleaned_NS.svg/_cropped_fortina.png}"
-	convert "${file/_cleaned_NS.svg/_cropped_fortina.png}" -resize '256x256>' -gravity center -background transparent -extent 264x264 "${file/_cleaned_NS.svg/_cropped_resized_fortina.png}"
+for file in ./*_cleaned.svg; do
+	echo "exporting $file to woman's folder"
+	svgexport "$file" ../../png_scaled_images/woman/"${file/_cleaned.svg/_fortina.png}" png 80% 9922:13000 992:1300
 done
+cd ../../png_scaled_images/man
+for file in ./*_fortina.png; do
+	echo "converting _fortina to a _cropped_fortine.svg file"
+	convert "${file}" -trim "${file/_fortina.png/_cropped_fortina.png}"
+	echo "converting _cropped_fortina.png file to a .png file"
+	convert "${file/_fortina.png/_cropped_fortina.png}" -resize '256x256>' -gravity center -background transparent -extent 264x264 "${file/_fortina.png/.png}"
+done
+cd ../woman
+for file in ./*_fortina.png; do
+	echo "$file"
+	convert "${file}" -trim "${file/_fortina.svg/_cropped_fortina.png}"
+	convert "${file/_fortina.svg/_cropped_fortina.png}" -resize '256x256>' -gravity center -background transparent -extent 264x264 "${file/_cropped_fortina.svg/.png}"
+done
+
